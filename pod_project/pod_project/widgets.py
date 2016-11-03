fd4728824f94cd4b473713bbfd9afb9d7eea28e3
AneoPsy <paultevatheis@gmail.com>
2016-11-03 09:56:43 -1000

    modifié :         pod_project/pod_project/widgets.py

--- pod_project/pod_project/widgets.py
+++ pod_project/pod_project/widgets.py
@@ -42,7 +42,7 @@ class SpaceWidget(KnobWidget):
 
     def get_value(self):
         space = self.disk_usage('/')
-        print(space.percent)
+        print(space)
         return 10
 
     def get_more_info(self):
