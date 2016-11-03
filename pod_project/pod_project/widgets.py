686c09365d7ae35497ef0fdf6bdd1c5f604857fb
AneoPsy <paultevatheis@gmail.com>
2016-11-02 14:55:18 -1000

    modifié :         pod_project/pod_project/widgets.py

--- pod_project/pod_project/widgets.py
+++ pod_project/pod_project/widgets.py
@@ -3,12 +3,13 @@ from dashing.widgets import NumberWidget
 from random import randint
 from django.contrib.auth.models import User
 
+
 class NewClientsWidget(NumberWidget):
     title = 'New Users'
 
     def get_value(self):
-        owners = User.objects.filter(pod__in=Pod.objects.filter(is_draft=False, encodingpods__gt=0).distinct()).order_by('last_name').distinct()
-        return len(owners)
+        owners = User.objects.distinct().count()
+        return owners
 
     def get_detail(self):
         return '{} actives'.format(3)
