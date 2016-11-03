e7a3dc150749263c0796e3a0b00073a15083572b
AneoPsy <paultevatheis@gmail.com>
2016-11-03 10:01:38 -1000

    modifié :         pod_project/pod_project/widgets.py

--- pod_project/pod_project/widgets.py
+++ pod_project/pod_project/widgets.py
@@ -46,7 +46,7 @@ class SpaceWidget(KnobWidget):
         return 10
 
     def get_more_info(self):
-        return str(self.disk_usage('/').free)
+        return str(self.disk_usage('/')[1])
 
 
 class ServerWidget(NumberWidget):
