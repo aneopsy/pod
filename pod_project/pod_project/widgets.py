8f8bc875ce61d589c79d3495dabf972d60f2d11a
AneoPsy <paultevatheis@gmail.com>
2016-11-22 12:49:05 -1000

    modifié :         pod_project/pod_project/widgets.py

--- pod_project/pod_project/widgets.py
+++ pod_project/pod_project/widgets.py
@@ -136,9 +136,10 @@ class UsersWidget(ListWidget):
         users = []
         values = []
         for user in User.objects.distinct():
-            users.append(user.username)
-            values.append(user.pod_set.filter(is_draft=False,
-                                              encodingpods__gt=0).distinct().count())
+            if user.is_staff:
+                users.append(user.username)
+                values.append(user.pod_set.filter(is_draft=False,
+                                                encodingpods__gt=0).distinct().count())
         return [{'label': x, 'value': y} for x, y in zip(users, values)]
 
 
