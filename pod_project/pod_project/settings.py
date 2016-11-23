aa0569458330e51f55c00e8a94a5ebc132f8fddb
AneoPsy <paultevatheis@gmail.com>
2016-11-22 15:26:58 -1000

    modifié :         pod_project/core/templates/header.html  	modifié :         pod_project/core/templates/navbar.html  	modifié :         pod_project/pod_project/settings.py

--- pod_project/pod_project/settings.py
+++ pod_project/pod_project/settings.py
@@ -128,7 +128,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 CKEDITOR_UPLOAD_PATH = os.path.join(MEDIA_ROOT, 'uploads')
 
 # Paramètres spécifiques au projet
-USE_EMAIL = TRUE
+USE_EMAIL = True
 EMAIL_HOST = 'ssmtp.upf.pf'
 EMAIL_PORT = 25
 DEFAULT_FROM_EMAIL = 'paultevatheis@gmail.com'
